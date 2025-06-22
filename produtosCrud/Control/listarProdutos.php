80c6f955c80e3fc8bfcb703744fdf7fc64a34b7e
Robson <tobikyoshiro@gmail.com>
2025-06-22 17:07:38 -0300

    Delete produtosCrud directory

--- produtosCrud/Control/listarProdutos.php
+++
@@ -1,8 +0,0 @@
-<?php   
-    require_once '../Model/DTO/ProdutosDTO.php';
-    require_once '../Model/DAO/ProdutosDAO.php';
-
-   $produtosDAO = new ProdutosDAO();
-   $todosProdutos=$produtosDAO->listarProdutos();
-
-?>
